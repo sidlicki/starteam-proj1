1ecc9dd4818990bead0c7f659af8a879649d601c
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-06-20 14:07:27 +0300

    виправив проблему з відображенням карток

--- src/js/swiper-loved.js
+++ src/js/swiper-loved.js
@@ -1,25 +1,27 @@
 const swiper = new Swiper('.swiper-loved', {
-    direction: 'horizontal',
-    loop: false,
-    initialSlide: 0,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+  direction: 'horizontal',
+  loop: false,
+  initialSlide: 0,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: true,
+  autoplaySpeed: 7500,
+  pauseOnHover: true,
+  pauseOnFocus: true,
+  pauseOnDotsHover: true,
+  waitForAnimate: false,
+  spaceBetween: 20, // Встановити відстань між картками
+  slidesPerView: 'auto',
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16, // Встановити відстань між картками
     },
-    autoplay: true,
-    autoplaySpeed: 7500,
-    pauseOnHover: true,
-    pauseOnFocus: true,
-    pauseOnDotsHover: true,
-    waitForAnimate: false,
-    variableWidth: true,
-    slidesPerView: 'auto',
-    breakpoints: {
-      768: {
-        slidesPerView: 2, // Відображати 2 елементи на екранах шириною 768px і більше
-      },
-      1200: {
-        slidesPerView: 3, // Відображати 3 елементи на екранах шириною 1200px і більше
-      },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28, // Встановити відстань між картками
     },
-  });
+  },
+});
